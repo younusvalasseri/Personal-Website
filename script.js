0284a9936afa16163a0f48e101c633e5b8847dd1
younusvalasseri <160461250+younusvalasseri@users.noreply.github.com>
2024-04-25 06:32:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -34,3 +34,29 @@ function validate() {
     }
 }
 
+
+
+// JavaScript
+function isInViewport(element) {
+    let rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function animateScrollCards() {
+    let cards = document.querySelectorAll('.scroll-down-animation');
+
+    cards.forEach(function (card) {
+        if (isInViewport(card)) {
+            card.classList.add('animation-active');
+        }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', animateScrollCards);
+window.addEventListener('scroll', animateScrollCards);
+
